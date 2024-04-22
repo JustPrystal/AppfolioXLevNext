a9811834338e628ab2fe6e1d8645e2879c4d1e67
Ali Sajid <ali.usmansajid02@gmail.com>
2024-04-22 13:53:14 -0700

    Final stuff

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const axios = require('axios'); // Import Axios
 const cors = require('cors'); // Import the cors middleware
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(cors()); // Use the cors middleware
 
